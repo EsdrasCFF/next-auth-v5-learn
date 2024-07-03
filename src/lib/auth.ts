6955841469d2cd5bf4eac914bde293cc0761aa9f
Esdras Castro <ecff.dev@gmail.com>
2024-07-02 23:30:36 -0300

    chore: define strategy jwt to auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,4 +6,7 @@ import { prisma } from "./prisma"
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [],
+  session: {
+    strategy: 'jwt'
+  }
 })
